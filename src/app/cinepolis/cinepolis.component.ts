3448f9cad0da5c902a82676897d74a9a82e4baaf
IDGS-802-20001492 <quirozbernardo2@gmail.com>
2023-06-26 21:00:07 -0600

    26/06/2023 9:00 pm
    26/06/2023

--- src/app/cinepolis/cinepolis.component.ts
+++ src/app/cinepolis/cinepolis.component.ts
@@ -9,5 +9,36 @@ export class CinepolisComponent {
   nBoletos!: number;
   nPersonas!: number;
   nombre!: string;
-  tarjeta!: boolean;
+  tarjeta!: number;
+  resultado!: string;
+
+  calcularTotal(): void {
+    let total = 0;
+
+    if(!(this.nBoletos > (this.nPersonas * 7))){
+      total = this.nBoletos * 12;
+      total = total - (total * this.calcularDescuento());
+
+      if(this.tarjeta == 1){
+        total = total - (total * 0.10);
+      }
+
+      this.resultado = this.nombre+" El total a pagar es: " + total;
+    }else{
+      this.resultado = this.nombre+" No se puede realizar la compra, solo 7 Boletos por persona";
+    }
+  }
+
+  calcularDescuento(): number {
+      let descuento = 0;
+    
+      if(this.nBoletos > 5){
+          descuento = 0.15;
+      }else if(this.nBoletos >= 3){
+          descuento = 0.10;
+      }else{
+          descuento = 0;
+      }
+      return descuento;
+  }
 }
